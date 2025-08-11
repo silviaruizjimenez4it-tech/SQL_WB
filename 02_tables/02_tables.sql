e068f7669190e0e2d94dd8c4ad5eb7f7af100b91
Peter <schmidt.multimedia@gmail.com>
2025-08-11 12:03:51 +0200

    02_tables / AUTO INCREMENT / PRIMARY KEY

--- 02_tables/02_tables.sql
+++ 02_tables/02_tables.sql
@@ -16,10 +16,11 @@ USE boo;
 DROP TABLE IF EXISTS kunde;
 
 -- Tabelle anlegen
-CREATE TABLE IF NOT EXISTS kunde
+CREATE TABLE IF NOT EXISTS kunde 
 (
+    k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(45),
-    k_alter TINYINT  
+    k_alter TINYINT
 );
 
 -- Tabellen anzeigen
@@ -29,8 +30,10 @@ SHOW TABLES;
 DESCRIBE kunde;
 
 -- Datensätze/Zeilen
-INSERT INTO kunde(k_name,k_alter) VALUES("Meier", 28);
-INSERT INTO kunde(k_name,k_alter) VALUES("Hussein", 32);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Meier", 28);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Hussein", 32);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Li", 26);
+INSERT INTO kunde(k_id,k_name,k_alter) VALUES(DEFAULT,"Li", 40);
 
 -- Tabellendaten (Inhalte) anzeigen
 SELECT * FROM kunde;
