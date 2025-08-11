f9e069d604344b7c58885b7e1b73f4abecc9c98c
Peter <schmidt.multimedia@gmail.com>
2025-08-11 11:14:42 +0200

    01_tables/ INSERT / SELECT

--- 02_tables/01_tables.sql
+++ 02_tables/01_tables.sql
@@ -25,3 +25,11 @@ SHOW TABLES;
 
 -- Struktur anzeigen
 DESCRIBE kunde;
+
+-- Datensätze/Zeilen
+INSERT INTO kunde(k_name,k_alter) VALUES("Meier", 28);
+INSERT INTO kunde(k_name,k_alter) VALUES("Hussein", 32);
+
+-- Tabellendaten (Inhalte) anzeigen
+SELECT * FROM kunde;
+
