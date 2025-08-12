962b4f4d82c54fc8281f321822ff8815e435225e
SilviaRJ <silviaruizjimenez4it@gmail.comgit config --listgit config --global user.email silviaruizjimenez4it@gmail.com>
2025-08-12 11:15:47 +0200

    Hash zugefügt

--- 02_tables/04_tables.sql
+++ 02_tables/04_tables.sql
@@ -1,4 +1,4 @@
--- UNIQUE
+-- Passwort anlegen / Hash 
 
 -- DB anlegen, falls noch nicht existent
 CREATE DATABASE IF NOT EXISTS boo;
@@ -8,7 +8,7 @@ USE boo;
 
 -- Tabelle löschen, falls existent
 -- Nur bei Test / Entwicklung
--- DROP TABLE IF EXISTS kunde;
+DROP TABLE IF EXISTS kunde;
 
 -- Tabelle anlegen
 CREATE TABLE IF NOT EXISTS kunde 
@@ -16,20 +16,21 @@ CREATE TABLE IF NOT EXISTS kunde
     k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(45) NOT NULL DEFAULT "TBA",
     k_alter TINYINT(4),
-    k_mail VARCHAR(100) UNIQUE
+    k_mail VARCHAR(100) UNIQUE,
+    k_pw VARCHAR(100) 
 );
 
 -- Struktur anzeigen
 DESCRIBE kunde;
 
 -- Datensätze/Zeilen
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Meier", 28, "meier@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Hussein", 32, "hussein@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Li", 26,"li@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Li", 40,"li2@web.de");
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,"Li", 62,"li_shanghai@web.de");
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Meier", 28, "meier@web.de",SHA2("meier1234",256));
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Hussein", 32, "hussein@web.de",SHA2("hus#!12",256));
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Li", 26,"li@web.de",SHA2("li1234",256));
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Li", 40,"li2@web.de",SHA2("#!aBcD",256));
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,"Li", 62,"li_shanghai@web.de",SHA2("wau12",256));
 
-INSERT INTO kunde(k_id,k_name,k_alter,k_mail) VALUES(DEFAULT,NULL,NULL,NULL);
+INSERT INTO kunde(k_id,k_name,k_alter,k_mail,k_pw) VALUES(DEFAULT,NULL,NULL,NULL);
 INSERT INTO kunde() VALUES();
 
 -- Tabellendaten (Inhalte) anzeigen
